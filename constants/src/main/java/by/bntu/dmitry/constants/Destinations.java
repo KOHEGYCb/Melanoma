6a8a470b8a98ff982a00d6b7df8d35df4f538a76
Dmitry <dimaushenko@mail.ru>
2018-10-16 18:59:14 +0300

    create web destinations file

---
+++ constants/src/main/java/by/bntu/dmitry/constants/Destinations.java
@@ -0,0 +1,12 @@
+package by.bntu.dmitry.constants;
+
+/**
+ *
+ * @author user
+ */
+public class Destinations {
+    
+    public static final String MAIN_PAGE = "/";
+    public static final String CREATE_USER_FORM_PAGE = "/createUserForm";
+    
+}
