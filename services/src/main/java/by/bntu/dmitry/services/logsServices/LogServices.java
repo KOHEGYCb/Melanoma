21e496c27df463f2d968a210c4f21e47a2697ae6
Dmitry <dimaushenko@mail.ru>
2018-10-16 11:27:01 +0300

    add log services

---
+++ services/src/main/java/by/bntu/dmitry/services/logsServices/LogServices.java
@@ -0,0 +1,33 @@
+package by.bntu.dmitry.services.logsServices;
+
+import by.bntu.dmitry.dao.LogsDAO;
+import by.bntu.dmitry.entities.Logs;
+import by.bntu.dmitry.entities.User;
+import by.bntu.dmitry.enums.LogActions;
+import java.sql.Date;
+
+/**
+ *
+ * @author user
+ */
+public enum LogServices {
+
+    INSTANCE;
+    
+    public void SignInLog(User user){
+        Logs log = new Logs();
+        log.setDate(new Date(System.currentTimeMillis()));
+        log.setUser(user);
+        log.setAction(LogActions.SIGN_IN);
+        LogsDAO.INSTANCE.createEntity(log);
+    }
+    
+    public void LogInLog(User user){
+        Logs log = new Logs();
+        log.setDate(new Date(System.currentTimeMillis()));
+        log.setUser(user);
+        log.setAction(LogActions.LOG_IN);
+        LogsDAO.INSTANCE.createEntity(log);
+    }
+    
+}
