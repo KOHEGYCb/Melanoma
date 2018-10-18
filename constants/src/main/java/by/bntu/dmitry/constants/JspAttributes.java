0074b42ecfd01d306da6427cba80a92a0acdb3b6
Dmitry <dimaushenko@mail.ru>
2018-10-19 02:52:48 +0300

    add jsp attributes constants

---
+++ constants/src/main/java/by/bntu/dmitry/constants/JspAttributes.java
@@ -0,0 +1,14 @@
+package by.bntu.dmitry.constants;
+
+/**
+ *
+ * @author user
+ */
+public class JspAttributes {
+
+    public static final String CREATE_USER_FORM_JSP_NAME = "name";
+    public static final String CREATE_USER_FORM_JSP_SURNAME = "surname";
+    public static final String CREATE_USER_FORM_JSP_PATRONYMIC = "patronymic";
+    public static final String CREATE_USER_FORM_JSP_BIRTHDAY = "birthday";
+    public static final String CREATE_USER_FORM_JSP_GENDER = "gender";
+}
