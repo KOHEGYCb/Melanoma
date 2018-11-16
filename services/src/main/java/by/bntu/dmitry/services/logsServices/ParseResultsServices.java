3ab1c851330fc631c531ea35ef063d8830a0232b
Dmitry <dimaushenko@mail.ru>
2018-11-16 13:02:20 -0700

    add result entity, add method to parse analysis file

---
+++ services/src/main/java/by/bntu/dmitry/services/logsServices/ParseResultsServices.java
@@ -0,0 +1,98 @@
+package by.bntu.dmitry.services.logsServices;
+
+import by.bntu.dmitry.constants.ConfigConstants;
+import by.bntu.dmitry.entities.Foto;
+import by.bntu.dmitry.entities.Result;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author user
+ */
+public class ParseResultsServices {
+
+    public static Result getResult(Foto foto) throws IOException {
+        System.out.println("\n\nStart Parse File");
+        Result result = null;
+
+        String fileName = "";
+        boolean isDotFind = false;
+        for (int i = foto.getDirectory().length() - 1; i >= 0; i--) {
+            if (isDotFind) {
+                fileName = foto.getDirectory().charAt(i) + fileName;
+            } else {
+                if (foto.getDirectory().charAt(i) == '.') {
+                    isDotFind = true;
+                }
+            }
+        }
+        String path = ConfigConstants.IMAGE_FOLDER + fileName + ".txt";
+
+        File file = new File(path);
+        if (file.isFile()) {
+            FileInputStream fis = null;
+            System.out.println("file is found");
+            result = new Result();
+            try {
+                fis = new FileInputStream(file);
+                boolean eof = false;
+                Map<String, String> map = new HashMap<>();
+                String param = "";
+                String key = "";
+                while (!eof) {
+                    int ch = fis.read();
+                    switch (ch) {
+                        case 13:
+                            map.replace(key, param);
+                            param = "";
+                            fis.read();
+                            break;
+                        case -1:
+                            eof = true;
+                            break;
+                        case 61:
+                            map.put(param, "");
+                            key = param;
+                            param = "";
+                            break;
+                        default:
+                            System.out.println(ch + " : " + (char) ch);
+                            param = param + (char) ch;
+                            break;
+                    }
+                }
+                result.setAbcde(ResultsServices.getABCDE(Double.valueOf(map.get("abcde"))));
+                result.setArg(Integer.valueOf(map.get("arg")));
+                result.setMenz(Integer.valueOf(map.get("menz")));
+                result.setProbability(ResultsServices.getProbability(Integer.valueOf(map.get("Probability"))));
+                result.setSimilarity(ResultsServices.getSimilarity(Integer.valueOf(map.get("Similarity"))));
+                ArrayList<String> sim = new ArrayList<>();
+                sim.add(map.get("Sim1"));
+                sim.add(map.get("Sim2"));
+                sim.add(map.get("Sim3"));
+                sim.add(map.get("Sim4"));
+                result.setSim(sim);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(ParseResultsServices.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (fis != null) {
+                    fis.close();
+                }
+            }
+        } else {
+            System.out.println("file is not found");
+        }
+
+        System.out.println("end.\n\n");
+        return result;
+    }
+
+}
